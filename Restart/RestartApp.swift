0e7017bfb68f62c98750733a4d584a473026011f
Alba Rico <amiloyrico.36@gmail.com>
2022-04-27 16:58:09 -0500

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Alba Yolima Rico Montenegro on 27/04/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
