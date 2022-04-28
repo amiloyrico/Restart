1d7e39fa4e61975fe75919e2ed531bc3fdbec232
Alba Rico <amiloyrico.36@gmail.com>
2022-04-27 20:00:53 -0500

    Creación del proyecto

--- Restart/Screens/ContentView.swift
+++ Restart/Screens/ContentView.swift
@@ -1,13 +1,16 @@
 //
 //  ContentView.swift
 //  Restart
-//
+
 //  Created by Alba Yolima Rico Montenegro on 27/04/22.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage ("onboarding") var isOnboardingViewActi: Bool = true
+    
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
---
+++ Restart/Screens/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Restart
+//
+//  Created by Alba Yolima Rico Montenegro on 27/04/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
