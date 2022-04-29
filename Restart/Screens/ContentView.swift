9aa16a7ef2209dcd3f0256de24cee3bf047eb810
Alba Rico <amiloyrico.36@gmail.com>
2022-04-28 22:07:10 -0500

    creación de menu y navegación

--- Restart/Screens/ContentView.swift
+++ Restart/Screens/ContentView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    @AppStorage ("onboarding") var isOnboardingViewActi: Bool = true
+    @AppStorage ("onboarding") var isOnboardingViewActive: Bool = true
     
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            if isOnboardingViewActive {
+                OnboardingView ()
+            } else {
+                HomeView ()
+            }
+        }
     }
 }
 
--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -8,8 +8,20 @@
 import SwiftUI
 
 struct HomeView: View {
+    // MARK:- PROPERTY
+    
+    @AppStorage ("onboarding") var isOnboardingviewActive: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing:20) {
+            Text("Home")
+                .font(.largeTitle)
+            
+            Button (action: {
+                isOnboardingviewActive = true
+            }) {
+              Text ("Restart")
+            }
+        } //: VSTACK
     }
 }
 
